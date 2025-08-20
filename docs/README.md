93a4e375b572064d656b28a34840a452fd9b5164
pokedot <110330779+pokedot@users.noreply.github.com>
2025-08-20 11:06:35 +0800

    Rename chatbot to FlyingNugget

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# FlyingNugget User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/FlyingNugget.java
@@ -0,0 +1,22 @@
+public class FlyingNugget {
+    public static void main(String[] args) {
+        String intro =  """
+            Hello from
+             _____ _       _             _   _                        _
+            |  ___| |_   _(_)_ __   __ _| \\ | |_   _  __ _  __ _  ___| |_
+            | |_  | | | | | | '_ \\ / _` |  \\| | | | |/ _` |/ _` |/ _ \\ __|
+            |  _| | | |_| | | | | | (_| | |\\  | |_| | (_| | (_| |  __/ |_
+            |_|   |_|\\__, |_|_| |_|\\__, |_| \\_|\\__,_|\\__, |\\__, |\\___|\\__|
+                     |___/         |___/             |___/ |___/
+            """;
+
+        String name = "FlyingNugget";
+        System.out.println(intro);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
