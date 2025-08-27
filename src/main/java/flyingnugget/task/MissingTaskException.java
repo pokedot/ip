36bd44df1267f282c1b6e6fdec2ef5e696821a50
pokedot <110330779+pokedot@users.noreply.github.com>
2025-08-27 19:48:18 +0800

    Update java file to follow coding standard

--- src/main/java/flyingnugget/task/MissingTaskException.java
+++ src/main/java/flyingnugget/task/MissingTaskException.java
@@ -1,7 +1,7 @@
 package flyingnugget.task;
 
 /**
- * Thrown when a user input is missing a task description or does not follow the required format for the command.
+ * Thrown when a user input is missing a task description or does not follow the required format for the task.
  */
 public class MissingTaskException extends Exception {
 }
